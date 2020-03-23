7d7bec3dd99bc672ded6a7d1f40a959f760619e6
Henry Adeyemi <youngach@gmail.com>
2020-03-23 18:50:48 +0100

    Adjust the ui from ui.js

--- ui.js
+++ ui.js
@@ -8,9 +8,9 @@ class UI {
       <div class="card card-body mb-3">
           <div class="row">
               <div class="col-md-3">
-                  <img class="img-fluid mb-2" src"${user.avata_url}">
-                  <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile </a>
-  
+                  <img class="img-fluid mb-2" src="${user.avatar_url}">
+                  <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile </a>
+                </div>
   
                   <div class="col-md-9">
                   <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
@@ -35,7 +35,6 @@ class UI {
   
                   </ul>
   
-                  </div>
   
               </div>
   
@@ -44,6 +43,9 @@ class UI {
   
   
       </div>
+
+      <h3 class="page-heading mb-3">Latest Repos</h3>
+      <div id="repos"></div>
       
       `;
   }
